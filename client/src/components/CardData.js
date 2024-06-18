c52de2258c3319d655eb482cc32195c7e17e61a3
Astha Tripathi <aaysatiwari92@gmail.com>
2024-06-18 22:56:19 +0530

    first commit

---
+++ client/src/components/CardData.js
@@ -0,0 +1,114 @@
+const Cardsdata = [
+    {
+        
+        id: 1,
+        dish: "Biryani",
+        imgdata: "https://biryani.hk/wp-content/uploads/2020/10/WhatsApp-Image-2020-10-22-at-6.01.48-AM.jpeg",        
+        address: "Best Biryani",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 135,
+        rating: "3.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+       
+    },
+    {
+        id: 2,
+        dish: "chole Bhature",
+        imgdata: "https://www.myyellowplate.com/wp-content/uploads/2021/06/Pind-Bhatura-in-Gurgaon-scaled.jpg",
+        address: "Street Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 105,
+        rating: "3.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        dish: "Pizza",
+        imgdata: "https://images7.alphacoders.com/596/596343.jpg",
+        address: "Pizza, Fast Food, Pasta",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 300,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        dish: "Momos",
+        imgdata: "https://3.bp.blogspot.com/-sEHt8dskG68/Vb0JF9uBIXI/AAAAAAAAB2M/VHZ4p5a_v8U/s1600/Egg-Momos-recipe-delicious-taste-1-5574-chickn1.jpg",
+        address: "Momos",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 150,
+        rating: "3.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        dish: "Indian Parathe",
+        imgdata: "https://www.justgotochef.com/img/1573227352-IMG_20190326_103020756_LL.jpg?w=815",
+        address: "North Indian",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 200,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        dish: "Spring Rolls",
+        imgdata: "https://jackfruitful.com/wp-content/uploads/2021/09/ACS_0234-1021x1024.jpg",
+        address: "Wraps FastFood, Chines",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 100,
+        rating: "3.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        dish: "Most Famous Indian Food",
+        imgdata: "https://th.bing.com/th/id/OIP.2uIVsBDIqHwFRVcGITgAlQHaEK?rs=1&pid=ImgDetMain",
+        address: "North Indian, Fast Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 400,
+        rating: "3.5",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        dish: "Indian Chai",
+        imgdata: "https://i.pinimg.com/originals/99/98/a0/9998a0804ee244b2c747dc522aec2d5a.jpg",
+        address: "Tea, Coffee, Shake, Beverages",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 200,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        dish: "Frankie",
+        imgdata: "https://b.zmtcdn.com/data/pictures/chains/7/19639627/c7d0790207fbf0c05c338c70b17830ec.jpg",
+        address: "Burger, Sandwich, Fast Food",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 150,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
